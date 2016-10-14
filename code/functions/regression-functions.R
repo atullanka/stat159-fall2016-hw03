ae44eb052b457e80d5dc4e7a887f0e453774c962
Atul Lanka <atul.lanka@berkeley.edu>
2016-10-14 04:29:17 -0700

    adding test functions

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,29 @@
+residual_sum_squares <- function(input) {
+  rss <- sum((input$residuals)^2)
+  return(rss)
+}
+
+total_sum_squares <- function(input) {
+  mean.input <- input$model[[1]] 
+  tss <- sum((input$residuals - mean.input)^2)
+  return(tss)
+}
+
+r_squared <- function(input) {
+  r_square <- 1 - (residual_sum_squares(input) / total_sum_squares(input))
+  return(r_square)
+}
+
+f_statistic <- function(input) {
+  p <- input$rank -1
+  n <- length(input$residuals)
+  f.stat <- (total_sum_squares(w) - residual_sum_squares(w)) * (n-p-1) / (p*(residual_sum_squares(w)))
+  return(f.stat)
+}
+
+residual_std_error <- function(input) {
+  p <- input$rank -1
+  n <- length(input$residuals)
+  rse <- sqrt(residual_sum_squares(input)/(n-p-1))
+  return(rse)
+}
