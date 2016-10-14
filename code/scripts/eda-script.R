fe175bf6f43f754c32b583fe9289483c3076e42e
Atul Lanka <atul.lanka@berkeley.edu>
2016-10-14 11:58:03 -0700

    edited the script files

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -2,16 +2,30 @@ advert <- read.csv('http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv')
 #Statistical summary
 summary.advert <- summary(advert[2:5])
 #Histograms
+
+png(file = "../../images/histogram-tv.png")
 hist(advert$TV, main = "TV Advertisement Budget (in thousands of dollars)", xlab = "Budget", col = "green")
+dev.off()
+
+png(file = "../../images/histogram-sales.png")
 hist(advert$Sales, main = "Sales (in thousands of units)", xlab = "Number of Sales", col = "red")
+dev.off()
+
+png(file = "../../images/histogram-radio.png")
 hist(advert$Radio, main = "Radio Advertisement Budget (in thousands of dollars)", xlab = "Budget", col = "blue")
+dev.off()
+
+png(file = "../../images/histogram-newspaper.png")
 hist(advert$Newspaper, main = "Newspaper Advertisement Budget (in thousands of dollars)", xlab = "Budget", col = "purple")
+dev.off()
 
 #Matrix of Correlation
 mat.cor <- cor(advert[2:5])
 
 #Scatterplot Matrix 
+png(file = "../../images/scatterplot-matrix.png")
 pairs(mat.cor, main = "Scatterplot Matrix")
+dev.off()
 
 #Saving the output to a .txt file
 sink("data/eda-output.txt")
--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -16,6 +16,7 @@ save(summary.lm.TV,summary.lm.radio, summary.lm.newspaper, summary.lm.all, file 
 
 #Scatterplots for TV, radio and newspaper
 
+png(file = "../../images/scatterplot-tv-sales.png")
 plot(advert$TV, advert$Sales, main = "Scatter plot with Regression line",
      xlab = 'TV advertising (in thousands of dollars)',
      ylab = 'Sales (in thousands of units)', pch = 20)
@@ -24,7 +25,9 @@ abline(lm.TV[[1]][[1]], lm.TV[[1]][[2]], col = 'blue', lwd = 1.5)
 #Adding the vertical lines between the points and the regression line to depict accuracy
 apply(cbind(advert$TV,advert$Sales,predict(lm.TV)),1,
       function(diff){lines(diff[c(1,1)],diff[2:3], col = 'red', lty = 2)})
+dev.off()
 
+png(file = "../../images/scatterplot-radio-sales.png")
 plot(advert$Radio, advert$Sales, main = "Scatter plot with Regression line",
      xlab = 'Radio advertising (in thousands of dollars)',
      ylab = 'Sales (in thousands of units)', pch = 20)
@@ -33,7 +36,9 @@ abline(lm.radio[[1]][[1]], lm.radio[[1]][[2]], col = 'blue', lwd = 1.5)
 #Adding the vertical lines between the points and the regression line to depict accuracy
 apply(cbind(advert$Radio,advert$Sales,predict(lm.radio)),1,
       function(diff){lines(diff[c(1,1)],diff[2:3], col = 'red', lty = 2)})
+dev.off()
 
+png(file = "../../images/scatterplot-newspaper-sales.png")
 plot(advert$Newspaper, advert$Sales, main = "Scatter plot with Regression line",
      xlab = 'Newspaper advertising (in thousands of dollars)',
      ylab = 'Sales (in thousands of units)', pch = 20)
@@ -45,5 +50,18 @@ apply(cbind(advert$Newspaper,advert$Sales,predict(lm.newspaper)),1,
 
 
 #Residual plot, QQ plot and Scale Location
-plot(lm.all, caption = list("Residuals vs Fitted", "Normal Q-Q","Scale-Location"))
+
+png(file = "../../images/residual-plot.png")
+plot(lm.all, which = 1)
+dev.off()
+
+
+png(file = "../../images/normal-qq-plot.png")
+plot(lm.all, which = 2)
+dev.off()
+
+
+png(file = "../../images/scale-location-plot.png")
+plot(lm.all, which = 3)
+dev.off()
 
